94e00a4a79bf3f195afcb654cfccfbd73bfe4644
VEERAJ SHENOY <veerajthegreat@gmail.com>
2017-07-17 00:34:30 +0530

    Animation List and Animations Implemented

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {Observable} from "rxjs/Observable";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    animatetype: string = '';
+    classhere: string = '';
 
-  constructor(public navCtrl: NavController) {
+    constructor() {
+    }
 
-  }
+    ionViewDidLoad() {
+        let promise = new Observable((observer) => {
+            document.getElementById("animatediv").addEventListener("animationend", function () {
+                observer.next(1);
+            });
+        });
+        promise.subscribe(res => this.classhere = '');
+    }
+
+
+    delaybeforeanimation() {
+        return new Promise((resolve, reject) => {
+            setTimeout(function () {
+                resolve('')
+            }, 500);
+        });
+    }
+
+    animate() {
+        this.delaybeforeanimation().then(res => {
+            this.classhere = this.animatetype + ' animated';
+        });
+    }
+
 
 }
